53e34f2f17ce9e0f09be18b5965180be139f9ed4
Sen Lin <33269903+rlinh99@users.noreply.github.com>
2019-02-07 00:26:12 -0800

    Initial commit

---
+++ Client.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import datetime
+# Replace with the correct URL
+url = "http://localhost:5000/user/getTime"
+
+# It is a good practice not to hardcode the credentials. So ask the user to enter credentials at runtime
+startTime = datetime.datetime.now()
+rsp = requests.get(url)
+endTime = datetime.datetime.now()
+#print (myResponse.status_code)
+
+# For successful API call, response code will be 200 (OK)
+if(rsp.ok):
+    # Loading the response data into a dict variable
+    # json.loads takes in only binary or string variables so using content to fetch binary content
+    # Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
+    jData = json.loads(rsp.content)
+
+    print("The response contains {0} properties".format(len(jData)))
+    print(jData)
+else:
+  # If response code is not ok (200), print the resulting http error code with description
+    rsp.raise_for_status()
---
+++ Server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import datetime
+
+app = Flask(__name__)
+api = Api(app)
+
+time = {"time": str(datetime.datetime.now())}
+
+
+class Time(Resource):
+    def get(self, request):
+        if request == "getTime":
+            return time["time"], 200
+        return "User not found", 404
+
+
+api.add_resource(Time, "/user/<string:request>")
